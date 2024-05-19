9a42dcd5d19dba959b2d35a4b43c767128e71999
vgmoura <vladmuravev.misc@gmail.com>
2024-05-19 14:17:42 +1100

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,13 @@
+#практическое задание по методу строк и организации программ
+name = "Владислав Муравьев"
+print(name[:])
+
+my_string = str(input())
+length = len(my_string)
+print(length)
+
+print(my_string.upper()) #вывод в верхнем регистре
+print(my_string.lower()) #вывод в нижнем регистре
+print(my_string.replace(" ","")) #удаление пробелов
+print(my_string[0]) #вывод первого символа
+print(my_string[-1]) #вывод последнего символа
